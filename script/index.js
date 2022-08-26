f95ddfa0fac372f50fdfb50dc7b3f1bbf86d96ba
Niraj Kumar <83273099+nirajraj98@users.noreply.github.com>
2022-08-26 21:23:15 +0530

    Delete script directory

--- script/index.js
+++
@@ -1,9 +0,0 @@
-import {slide,slide2}  from "../component/slider.js"
-var doo=document.getElementById("slidingBanner-1")
-var doo2=document.getElementById("slidingBanner-2")
-doo2.innerHTML = slide2();
-doo.innerHTML=slide()
-// doo.innerHTML=slidejs()
-
-//3rd
-
