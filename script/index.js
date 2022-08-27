3a8bfc49787142c655bb8668a2760896ba6fac08
ajaysingh <vajaysingh2002@gmail.com>
2022-08-27 15:04:27 +0530

    merge

--- script/index.js
+++ script/index.js
@@ -1,6 +1,10 @@
 import slide  from "../component/slider.js"
 var doo=document.getElementById("slidingBanner-1")
 
+import { nav, time} from "../navbar/components/nav.js"
+document.getElementById("navbar").innerHTML=nav()
+
+
 doo.innerHTML=slide()
 // doo.innerHTML=slidejs()
 
